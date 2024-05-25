cb3f72f0d94dd366407a87c11f01ee8a0ec6ef7b
hoqueJ <hoquej.dev@gmail.com>
2024-05-25 07:59:52 +0100

    feat: [86bywrtqp] - Create a Navbar Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className="text-3xl text-blue-400">My Portfolio</h1>
+    <main>
+      <h1 className="text-3xl text-blue-400">Jubayedul Hoque</h1>
     </main>
   );
 }
