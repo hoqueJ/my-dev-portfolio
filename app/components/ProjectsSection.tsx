bcba9b1679cca426bed9a53a9519e479ff65d24c
hoqueJ <hoquej.dev@gmail.com>
2024-05-31 10:10:15 +0100

    feat: [86bywrver] - Create a Projects Section

---
+++ app/components/ProjectsSection.tsx
@@ -0,0 +1,9 @@
+
+export default function ProjectsSection() {
+  return (
+    <main className="text-center">
+        <h1 className="mb-5">Projects</h1>
+        <hr className="my-4 border-black"/>
+    </main>
+)
+}
